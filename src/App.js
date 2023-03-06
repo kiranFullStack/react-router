941a178032fbbf8d0b3c58c4b25b866dd3f0dabb
kiranFullStack <kiran.fullstack@outlook.com>
2023-03-06 19:14:01 +0530

    added notfound and page nesting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Notfound from './Notfound'
 import Page1 from './Page1'
 import Page2 from './Page2'
 import Page3 from './Page3'
@@ -7,9 +9,32 @@ function App() {
   return (
     <div className='App'>
       <h1>React Router Starter</h1>
-      <Page1 />
-      <Page2 />
-      <Page3 />
+      <Routes>
+        <Route
+          path='/'
+          element={<Page1 />}
+        />
+        <Route
+          path='/page2'
+          element={<Page2 />}
+        />
+        <Route
+          path='/page2/subpage'
+          element={<Page2 />}
+        />
+        <Route
+          path='/page2/subpage/sub-subpage'
+          element={<Page2 />}
+        />
+        <Route
+          path='/page3'
+          element={<Page3 />}
+        />
+        <Route
+          path='*'
+          element={<Notfound />}
+        />
+      </Routes>
     </div>
   )
 }
---
+++ src/Notfound.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Notfound() {
+  return (
+    <div>
+      <h1>Notfound</h1>
+    </div>
+  )
+}
